8e3a197e43bada4257adf013ad15f3334c2c47a7
王苏云 <92081796+sylearn@users.noreply.github.com>
2025-08-30 13:44:26 +0800

    重构分类

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,9 +28,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '知识笔记', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '实用教程', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '闲谈随笔', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '学习札记', url: '/tag/学习札记' },
+  HEO_HERO_CATEGORY_2: { title: '工具方法', url: '/tag/工具方法' },
+  HEO_HERO_CATEGORY_3: { title: '生活点滴', url: '/tag/生活点滴' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
